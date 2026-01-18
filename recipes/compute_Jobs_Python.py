972c4bcdeb2db5c5464b4a9b745c7c9f014107ea
seokyukim98@gmail.com <seokyukim98@gmail.com>
2026-01-18 18:13:55 +0000

    Recipe FRAUDULENTJOBPOSTINGSCLASSIFICATION.compute_Jobs_Python saved through notebook notebook editor for compute_Jobs_Python

--- recipes/compute_Jobs_Python.py
+++ recipes/compute_Jobs_Python.py
@@ -1,3 +1,6 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+import dataiku
+from snowflake.snowpark.functions import *
 from dataiku.snowpark import DkuSnowpark
 
 # Create the DSS wrapper around Snowpark
@@ -7,13 +10,15 @@ dku_snowpark = DkuSnowpark()
 input_dataset = dataiku.Dataset("JOBS_POSTINGS_JOINED_prepared")
 input_dataset_df = dku_snowpark.get_dataframe(input_dataset)
 
-# TODO: Replace this part by your actual code that computes the output, as a Snowpark dataframe
-# For this sample code, simply copy input to output
-output_dataset_df = input_dataset_df
+#strip minimum salary from the given range
+output_dataset_df = input_dataset_df.withColumn('"MIN_SALARY"', split(col('"SALARY_RANGE"'), lit('-'))[0])
 
 # Write outputs
 output_dataset = dataiku.Dataset("Jobs_Python")
 dku_snowpark.write_with_schema(output_dataset, output_dataset_df)
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+from snowflake.snowpark import Session
 
-
+session = dku_snowpark.get_session(connection_name="Snowflake_Connection")
+print(session.get_current_database())
