cfd2d90ab6926e469db1360c911fe124b61a1bdb
seokyukim98@gmail.com <seokyukim98@gmail.com>
2026-01-18 18:04:29 +0000

    Saved recipe: FRAUDULENTJOBPOSTINGSCLASSIFICATION.compute_Jobs_Python

--- recipes/compute_Jobs_Python.py
+++ recipes/compute_Jobs_Python.py
@@ -1,6 +1,19 @@
-import dataiku
-from snowflake.snowpark import Session
+from dataiku.snowpark import DkuSnowpark
 
-print("Success! Snowflake libraries are found.")
+# Create the DSS wrapper around Snowpark
+dku_snowpark = DkuSnowpark()
+
+# Read inputs
+input_dataset = dataiku.Dataset("JOBS_POSTINGS_JOINED_prepared")
+input_dataset_df = dku_snowpark.get_dataframe(input_dataset)
+
+# TODO: Replace this part by your actual code that computes the output, as a Snowpark dataframe
+# For this sample code, simply copy input to output
+output_dataset_df = input_dataset_df
+
+# Write outputs
+output_dataset = dataiku.Dataset("Jobs_Python")
+dku_snowpark.write_with_schema(output_dataset, output_dataset_df)
+
 
 
